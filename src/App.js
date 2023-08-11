2f8d5bbed27787683ee11863ebefff93e9a13656
Yanick SOMBLE <sombleyannick@yahoo.fr>
2023-08-11 21:40:52 +0200

    create the header component

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
+import Header from './components/Header';
 function App() {
   return (
     <>
       <Router>
-       <Routes>
-          <Route path='/'element={<Home/>}/>
-          <Route path='/profile'element={<Profile/>}/>
-          <Route path='/sign-in'element={<SignIn/>}/>
-          <Route path='/sign-up'element={<SignUp/>}/>
-          <Route path='/forgot-password'element={<ForgotPassword/>}/>
-          <Route path='/offers'element={<Offers/>}/>
-       </Routes>
+        <Header/>
+         <Routes>
+            <Route path='/'element={<Home/>}/>
+            <Route path='/profile'element={<Profile/>}/>
+            <Route path='/sign-in'element={<SignIn/>}/>
+            <Route path='/sign-up'element={<SignUp/>}/>
+            <Route path='/forgot-password'element={<ForgotPassword/>}/>
+            <Route path='/offers'element={<Offers/>}/>
+          </Routes>
       </Router>
     </>
   );
